abf002afe2954df7ca71170620d98e2f5ee795c0
Kaylem/Kaijubear <Kaijubear@users.noreply.github.com>
2017-09-14 01:05:30 +1200

    Movement v2
    Cleaned up movement and blend trees

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour {
 
     private Animator anim;
 
+    private bool isMoving;
+    private Vector2 lastMove;
+
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
@@ -17,17 +20,27 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        isMoving = false;
+
         //Player Movement
         if (Input.GetAxisRaw("Horizontal") > 0f || Input.GetAxisRaw("Horizontal") < 0f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime,0f,0f));
+            isMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"),0f);
         }
         if (Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < 0f)
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            isMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+
         }
 
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("isMoving",isMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
 	}
 }
