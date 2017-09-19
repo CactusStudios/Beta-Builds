249573d232c0b34d90a7def5791d2dbb7a441d0c
Carmiej <blake.mclean@hotmail.com>
2017-09-20 09:40:49 +1200

    Added bullet to player
    Still need to work on the direction for the bullet

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
     //private float moveSpeed = 1.0f;
 
     private Animator anim;
@@ -42,5 +44,25 @@ public class PlayerController : MonoBehaviour {
         anim.SetBool("isMoving",isMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.y);
-	}
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
 }
