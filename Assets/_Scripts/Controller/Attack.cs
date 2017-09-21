e06df8bf1c59676149a46ba1f9a30d92d8238fd5
Kaijubear <Kaijubear@users.noreply.github.com>
2017-09-21 16:39:40 +1200

    Camera Tracking

---
+++ Assets/_Scripts/Controller/Attack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+	public float attackSpeed;
+	private float lastShotTime;
+
+    // Use this for initialization
+    void Start () {
+		lastShotTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+		if(Time.time > (attackSpeed*Time.deltaTime)+lastShotTime){
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+		
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+			lastShotTime = Time.time;
+	}
+
+
+    }
+}
