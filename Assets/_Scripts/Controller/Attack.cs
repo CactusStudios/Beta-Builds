022f7614b79375dba4cd4b79e941e66c5f58fea8
Kaijubear <Kaijubear@users.noreply.github.com>
2017-09-23 18:23:40 +1200

    Bullet v3
    Updated the bullet sprite, movement, destruction and rotation for cleaner firing

--- Assets/_Scripts/Controller/Attack.cs
+++ Assets/_Scripts/Controller/Attack.cs
@@ -7,7 +7,9 @@ public class Attack : MonoBehaviour {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
 	public float attackSpeed;
+	public int bulletSpeed;
 	private float lastShotTime;
+	public float lifeTime;
 
     // Use this for initialization
     void Start () {
@@ -16,31 +18,39 @@ public class Attack : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.Space))
-        {
-            Fire();
-        }
-    }
+		if (Input.GetMouseButton(0))
+		{
 
-    void Fire()
-    {
-		if(Time.time > (attackSpeed*Time.deltaTime)+lastShotTime){
-        // Create the Bullet from the Bullet Prefab
-        var bullet = (GameObject)Instantiate(
-            bulletPrefab,
-            bulletSpawn.position,
-            bulletSpawn.rotation);
-		
+			Vector2 target = Camera.main.ScreenToWorldPoint (new Vector2 (Input.mousePosition.x, Input.mousePosition.y));
 
-        // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+			Vector2 myPosition = new Vector2 (transform.position.x, transform.position.y);
+
+			Vector2 direction = target - myPosition;
+			direction.Normalize ();
+
+			Quaternion rotation = Quaternion.Euler (0, 0, Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg-45);
+
+			
+			if (Time.time > (attackSpeed * Time.deltaTime) + lastShotTime) {
+				// Create the Bullet from the Bullet Prefab
+				GameObject bullet = (GameObject)Instantiate (
+					bulletPrefab,
+					myPosition, rotation);
+				
 
 
-        // Destroy the bullet after 2 seconds
-        Destroy(bullet, 2.0f);
+				// Add velocity to the bullet
+				bullet.GetComponent<Rigidbody2D> ().velocity = direction/*Input.mousePosition*/ * bulletSpeed;
+
+
+			// Destroy the bullet after 2 seconds
+
+			Destroy (bullet, lifeTime);
 			lastShotTime = Time.time;
-	}
 
-
+        	}
+    	}
     }
+
+    
 }
