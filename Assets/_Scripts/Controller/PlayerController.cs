f91aead3b160cd8927e5d849ab80380c9da7ef6b
Kaijubear <Kaijubear@users.noreply.github.com>
2017-09-24 19:09:10 +1300

    Game v1
    A playable game
    
    -Collision detection for landscape
    -RG landscape
    -Bullet firing
    -Camera Tracking
    -Animation
    -Light polishing on landscape assets
    -Preset layers, tags and IDs

--- Assets/_Scripts/Controller/PlayerController.cs
+++ Assets/_Scripts/Controller/PlayerController.cs
@@ -12,11 +12,13 @@ public class PlayerController : MonoBehaviour {
 
 
     private bool isMoving;
+	private bool canMove;
     private Vector2 lastMove;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+		canMove = true;
 
 	}
 	
@@ -26,20 +28,23 @@ public class PlayerController : MonoBehaviour {
         isMoving = false;
 
         //Player Movement
-        if (Input.GetAxisRaw("Horizontal") > 0f || Input.GetAxisRaw("Horizontal") < 0f)
-        {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime,0f,0f));
-            isMoving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"),0f);
-        }
-        if (Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < 0f)
-        {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-            isMoving = true;
-            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
 
-        }
+		if (canMove) {
 
+			if (Input.GetAxisRaw ("Horizontal") > 0f || Input.GetAxisRaw ("Horizontal") < 0f) {
+				transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+				isMoving = true;
+				lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
+			}
+			if (Input.GetAxisRaw ("Vertical") > 0f || Input.GetAxisRaw ("Vertical") < 0f) {
+				transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+				isMoving = true;
+				lastMove = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
+
+			}
+		}
+
+		//sets direction for when the player stops moving
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
         anim.SetBool("isMoving",isMoving);
@@ -48,5 +53,7 @@ public class PlayerController : MonoBehaviour {
 
     }
 
-
 }
+
+
+
