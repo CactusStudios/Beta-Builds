bc511c640e4b044160248663a1f96e0866e91de1
Cain <cain.rawlinson@gmail.com>
2017-10-30 17:40:02 +1300

    Update Dungeon Gen
    Room class added, gets the layout from XML text that's passed in

---
+++ Assets/_Scripts/Model/Room.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Xml;
+using System.Collections;
+
+public class Room
+{
+    public Room (XmlNode thisRoom, bool n, bool s, bool e, bool w)
+    {
+        string whatToMake = "Creating room with layout:\n" +
+            thisRoom["LAYOUT"].InnerText;
+        Debug.Log(whatToMake);
+    }
+}
